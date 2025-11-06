c1d56fdf1e07464677dfb18002759b4c83d41005
SoumyadipDutta1004 <soumyadipdutta1004@gmail.com>
2025-11-06 16:24:20 +0530

    feat(auth): add dedicated login and sign-up pages

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,14 +1,3 @@
-import AuthForm from '@/components/auth/auth-form';
-
 export default function HomePage() {
-  return (
-    <main>
-      <section className='container mx-auto h-screen w-full bg-background'>
-        <div className='flex h-full w-full items-center justify-center gap-4'>
-          <AuthForm formType='login' />
-          <AuthForm formType='sign-up' />
-        </div>
-      </section>
-    </main>
-  );
+  return <div>HomePage</div>;
 }
